a9d128225c111318ad5e229f330e941884766af9
BB <biring@gmail.com>
2026-03-01 22:59:37 -0500

    chore(constants): centralize shared directory and file naming constants
    - Add common folder constants for datasets and reports
    
    - Define standard file suffixes for signal and annotation inputs

---
+++ src/constants.py
@@ -0,0 +1,11 @@
+"""
+This module defines constants used throughout the sleep data analysis pipeline.
+"""
+
+# --- folder constants ---
+SLEEP_DATA_DIR = "data/physionet-sleep-data"
+REPORTS_DIR = "data/reports"
+
+# --- file constants ---
+EDF_FILE_SUFFIX = "-PSG.edf"
+HYPNOGRAM_FILE_SUFFIX = "-Hypnogram.edf"
